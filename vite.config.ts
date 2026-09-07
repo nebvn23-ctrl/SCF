f3665d5ff5f394e891a9cd66746c5f5afecb9c6c
nebvn23-ctrl <nebvn23@gmail.com>
2026-09-07 19:40:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
    *   base: '/repo-name/',
    * Every media path is built from this value, so that one line is all it takes.
    */
-  base: '/smoking-chicken-fish/',
+  base: '/',
 })
